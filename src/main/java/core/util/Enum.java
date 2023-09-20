28e5329073f07ee7f0c48f9c6f67c10a22b42499
tuan.pham <tuan.pham@logigear.com>
2023-09-20 16:26:06 +0700

    add enum file

---
+++ src/main/java/core/util/Enum.java
@@ -0,0 +1,16 @@
+package core.util;
+
+public class Enum {
+    public enum QuestionType {
+        CHECKBOX,
+        CHECKBOX_GRID,
+        DATE,
+        DROP_DOWN,
+        LINEAR_SCALE,
+        MULTIPLE_CHOICE,
+        MULTIPLE_CHOICE_GRID,
+        PARAGRAPH,
+        SHORT_ANSWER,
+        TIME
+    }
+}
