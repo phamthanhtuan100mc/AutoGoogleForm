d50d3d4821a61bd89918a7049ceabf174e57036a
tuan.pham <tuan.pham@logigear.com>
2023-10-17 16:23:23 +0700

    format source

--- src/test/java/testcase/e2e_test/EndToEnd.java
+++ src/test/java/testcase/e2e_test/EndToEnd.java
@@ -15,6 +15,7 @@ public class EndToEnd extends BaseTest {
     private static final Logger log = LogManager.getLogger(EndToEnd.class);
     HomePage homePage;
     List<Question> questionList;
+
     @Test()
     public void TC_E2E() {
         homePage = new HomePage();
